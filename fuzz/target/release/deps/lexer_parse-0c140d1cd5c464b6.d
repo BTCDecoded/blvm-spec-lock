85ae6e1d799245aa879b5bd5fc981aad900616f9
Josh <securesovereigns@pm.me>
2026-08-18 04:43:47 -0400

    peel: ignore and drop fuzz/target artifacts (S2)
    Nested fuzz workspace target was tracked; gitignore + staged deletions land together.

--- fuzz/target/release/deps/lexer_parse-0c140d1cd5c464b6.d
+++
@@ -1,5 +0,0 @@
-/mnt/data/bitcoin/blvm/blvm-spec-lock/fuzz/target/release/deps/lexer_parse-0c140d1cd5c464b6.d: fuzz_targets/lexer_parse.rs
-
-/mnt/data/bitcoin/blvm/blvm-spec-lock/fuzz/target/release/deps/lexer_parse-0c140d1cd5c464b6: fuzz_targets/lexer_parse.rs
-
-fuzz_targets/lexer_parse.rs:
--- fuzz/target/release/deps/libfuzzer_sys-9c2560e35f99749b.d
+++
@@ -1,7 +0,0 @@
-/mnt/data/bitcoin/blvm/blvm-spec-lock/fuzz/target/release/deps/libfuzzer_sys-9c2560e35f99749b.d: /home/josh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs
-
-/mnt/data/bitcoin/blvm/blvm-spec-lock/fuzz/target/release/deps/liblibfuzzer_sys-9c2560e35f99749b.rlib: /home/josh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs
-
-/mnt/data/bitcoin/blvm/blvm-spec-lock/fuzz/target/release/deps/liblibfuzzer_sys-9c2560e35f99749b.rmeta: /home/josh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs
-
-/home/josh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libfuzzer-sys-0.4.12/src/lib.rs:
